b2c097d1c7884b9105a102a9b6cfcbb9ed9a03ed
Andrew Schwimmer <andrew.schwimmer@disneystreaming.com>
2023-02-22 15:47:40 -0500

    added github actions

--- src/main/java/schwimmer/projectile/Projectile.java
+++ src/main/java/schwimmer/projectile/Projectile.java
@@ -2,6 +2,8 @@ package schwimmer.projectile;
 
 public class Projectile {
 
+    private static final double GRAVITY = 9.8;
+
     private final double angle;
     private final double velocity;
     private final double radians;
@@ -12,20 +14,20 @@ public class Projectile {
         this.radians = Math.toRadians(angle);
     }
 
-    public double getAngle() {
-        return angle;
-    }
-
     public double getVelocity() {
         return velocity;
     }
 
+    public double getAngle() {
+        return angle;
+    }
+    
     public double getX(double time) {
         return Math.cos(radians) * velocity * time;
     }
 
     public double getY(double time) {
-        return Math.sin(radians) * velocity * time - .5 * 9.8 * time * time;
+        return Math.sin(radians) * velocity * time - .5 * GRAVITY * time * time;
     }
 
 }
